3f2479ab6705cb3349348df263a57f4ab5ad3dac
Jonathanlemon <43302879+Jonathanlemon@users.noreply.github.com>
2020-04-15 21:58:09 -0400

    Update riemannsum.js

--- scripts/riemannsum.js
+++ scripts/riemannsum.js
@@ -2,7 +2,7 @@ var x;
 var y;
 var xIncrement;
 var slider;
-var sliderVal;
+var sliderVal=0;
 var area;
 var drawArea;
 var redone=1;
@@ -15,30 +15,6 @@ function setup() {
   slider = createSlider(4, 80, 4, 4);
   slider.position(50, 450);
   slider.style('width', '400px');
-  noStroke();
-  fill(255,255,255);
-  textSize(32);
-  text("Mr. Johnston's Riemann Sum Animation", 50, 50);
-  textSize(32);
-  text("As the number of rectangles approaches infinity and the width of each rectangle approaches 0, the sum of the areas of each rectangle become closer and closer to the actual area of the curve.", 50, 100,600,600);
-  textSize(20);
-  fill(0,255,0);
-  text("Drag the slider to increase the number of rectangles", 50, 340,600,600);
-  text("Area : ", 50,380);
-  
-  fill(255,255,255);
-  textSize(15);
-  text(0,0,815);
-  text(1,800,815);
-  text(1,810,15);
-  textSize(32)
-  text("This curve is x on the interval [0,1]", 50,850);
-  textSize(16)
-  text("2", 255,825);
-  strokeWeight(1);
-  stroke(255,255,255);
-  line(0,800,800,800);
-  line(800,800,800,0);
 }
 
 function draw() {
@@ -47,14 +23,14 @@ function draw() {
     sliderVal=slider.value();
   }
   
-  if(x<800){
+  if(x<799){
 	  strokeWeight(1);
 	  stroke(255,255,255);
 	  fill(255,0,0);
 	  rect(x,800-y,xIncrement,y);
+	  area+=(xIncrement/800)*(y/800);
 	  x+=xIncrement;
 	  y=800*(pow(x/800, 2.0));
-	  area+=xIncrement/800*y/800;
   }
   else{
 	  drawArea=1;
@@ -62,7 +38,8 @@ function draw() {
   if(drawArea==1&&redone==1){
 	noStroke();
 	fill(0,255,0);
-	text(area, 130,380);
+	textSize(20);
+	text(area, 120,390);
 	redone=0;
   }
   
@@ -78,11 +55,11 @@ function redo(){
   textSize(32);
   text("Mr. Johnston's Riemann Sum Animation", 50, 50);
   textSize(32);
-  text("As the number of rectangles approaches infinity and the width of each rectangle approaches 0, the sum of the areas of each rectangle approaches the actual area of the curve.", 50, 100,600,600);
+  text("As the number of rectangles approaches infinity and the width of each rectangle approaches 0, the sum of the areas of each rectangle approaches the actual area under the curve. \n(0.333 in this case)", 50, 100,600,600);
   textSize(20);
   fill(0,255,0);
-  text("Drag the slider to increase the number of rectangles", 50, 340,600,600);
-  text("Area : ", 50,380);
+  text("Drag the slider to increase the number of rectangles", 50, 350,600,600);
+  text("Area : ", 50,390);
 
   fill(255,255,255);
   textSize(15);
@@ -90,9 +67,9 @@ function redo(){
   text(1,800,815);
   text(1,810,15);
   textSize(32)
-  text("This curve is x on the interval [0,1]", 50,850);
-  textSize(16)
-  text("2", 255,825);
+  text("f(x)=x    on the interval [0,1]", 50,540);
+  textSize(14)
+  text("2", 132, 522);
   strokeWeight(1);
   stroke(255,255,255);
   line(0,800,800,800);
